9254c8e9c86572764a077d2b2070fc3578bc2ef9
IIITManjeet <96388375+IIITManjeet@users.noreply.github.com>
2022-11-19 12:53:29 +0530

    Added posts schema and routes for posts

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const userRoute=require("./routes/users.js")
+const userRoute = require("./routes/users.js")
 const authRoute = require("./routes/auth.js")
+const postRoute=require("./routes/posts.js")
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, () => {
@@ -15,8 +16,9 @@ mongoose.connect(process.env.MONGO_URL, () => {
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-app.use("/api/user",userRoute);
+app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
  console.log("Backend server is running")
