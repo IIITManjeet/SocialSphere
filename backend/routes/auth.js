969076a0165df3b31a7c32d1f61316534b9a2769
IIITManjeet <96388375+IIITManjeet@users.noreply.github.com>
2022-11-19 11:52:54 +0530

    second commit to auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,39 @@
 const router = require("express").Router();
-const User=require("../models/User")
+const User=require("../models/User");
+const bcrypt=require("bcrypt");
 
-router.get("/register",async (req, res) => {
- const user=await new User({
-  username:"John",
-  email:"john@gmail.com",
-  password:"123456"
- })
+router.post("/register",async (req, res) => { 
+ try{
+  const salt=await bcrypt.genSalt(10);
+  const hashedPassword=await bcrypt.hash(req.body.password,salt);
 
- await user.save()
- res.send("Ok")
+  const newUser = new User({
+   username: req.body.username,
+   email: req.body.email,
+   password: hashedPassword
+  });
+
+  const user=await newUser.save();
+  res.status(200).json(user);
+ }catch(err){
+  res.status(500).json(err);
+ }
+})
+
+router.post("/login",async (req,res)=>{
+ try{
+  const user = await User.findOne({
+   email: req.body.email
+  });
+  !user && res.status(404).json("User not found");
+  const validPassword=await bcrypt.compare(req.body.password,user.password)
+  !validPassword && res.status(400).json("wrong Password")
+
+  res.status(200).json(user);
+ }catch(err){
+  res.status(500).json(err);
+ }
+
 })
 
 module.exports = router
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,5 @@
 const router =require("express").Router();
+const User=require("../models/User");
 
 router.get("/",(req,res)=>{
  res.send("Hey it's users routes")
